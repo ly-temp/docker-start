84fb221ccc6b9d02c1a71040fb2c6ab08a02b691
ly-temp <100673133+ly-temp@users.noreply.github.com>
2024-03-15 22:42:42 +0000

    1

--- buildx.sh
+++ buildx.sh
@@ -1,10 +1,11 @@
 #!/bin/sh -e
-CLOUD_NAME="$1"
+CLOUD_NAME=$1
 ARCH=amd64
 
-sudo apt update && sudo apt install -y jq curl
+(sudo apt update && sudo apt install -y jq curl)>/dev/null 2>&1
 mkdir -p ~/.docker/cli-plugins
 BUILDX_URL=$(curl -s https://raw.githubusercontent.com/docker/actions-toolkit/main/.github/buildx-lab-releases.json | jq -r ".latest.assets[] | select(endswith(\"linux-$ARCH\"))")
+echo "$BUILDX_URL"
 curl --silent -L --output ~/.docker/cli-plugins/docker-buildx $BUILDX_URL
 chmod a+x ~/.docker/cli-plugins/docker-buildx
 docker buildx create --use --driver cloud "$CLOUD_NAME"
